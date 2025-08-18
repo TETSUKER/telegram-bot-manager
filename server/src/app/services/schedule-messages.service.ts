11d2d4bac686054452df06d4ab1e30a1689d0253
TETSUKER <romantokmin.dev@gmail.com>
2025-08-18 21:32:55 +0700

    [Server] Bug - job'ы дублируются

--- server/src/app/services/schedule-messages.service.ts
+++ server/src/app/services/schedule-messages.service.ts
@@ -1,14 +1,14 @@
-import { diContainer } from 'app/core/di-container';
-import { EventBus } from 'app/core/event-bus';
-import { Logger } from 'app/core/logger';
-import { EventName } from 'app/interfaces/event-bus.interfaces';
-import { DayOfWeek, Rule, RuleResponse } from 'app/interfaces/rule.interfaces';
-import schedule, { Job, RecurrenceRule } from 'node-schedule';
-import { RulesService } from './rules.service';
-import { BotsService } from './bots.service';
-import { ChatsService } from './chats.service';
-import { Bot } from 'app/interfaces/bot.interfaces';
-import { MessageResponseService } from './message-response.service';
+import { diContainer } from "app/core/di-container";
+import { EventBus } from "app/core/event-bus";
+import { Logger } from "app/core/logger";
+import { EventName } from "app/interfaces/event-bus.interfaces";
+import { DayOfWeek, Rule, RuleResponse } from "app/interfaces/rule.interfaces";
+import schedule, { Job, RecurrenceRule } from "node-schedule";
+import { RulesService } from "./rules.service";
+import { BotsService } from "./bots.service";
+import { ChatsService } from "./chats.service";
+import { Bot } from "app/interfaces/bot.interfaces";
+import { MessageResponseService } from "./message-response.service";
 
 export class ScheduleMessagesService {
   private jobs = new Map<string, Job>();
@@ -19,33 +19,41 @@ export class ScheduleMessagesService {
     private eventBus: EventBus,
     private botsService: BotsService,
     private chatsService: ChatsService,
-    private messageResponseService: MessageResponseService,
+    private messageResponseService: MessageResponseService
   ) {
-    this.eventBus.subscribe(EventName.added_rules_to_bot, async ({ ruleIds, botId }) => {
-      const rules = await this.rulesService.getRules({ ids: ruleIds });
-      const [bot] = await this.botsService.getBots({ ids: [botId] });
+    this.eventBus.subscribe(
+      EventName.added_rules_to_bot,
+      async ({ ruleIds, botId }) => {
+        const rules = await this.rulesService.getRules({ ids: ruleIds });
+        const [bot] = await this.botsService.getBots({ ids: [botId] });
+        if (bot) {
+          for (const rule of rules) {
+            this.scheduleMessage(rule, bot);
+          }
+        }
+      }
+    );
+    this.eventBus.subscribe(
+      EventName.removed_rules_from_bot,
+      ({ ruleIds, botId }) => {
+        for (const ruleId of ruleIds) {
+          const jobId = this.getJobId(ruleId, botId);
+          this.cancelScheduledMessage(jobId);
+        }
+      }
+    );
+    this.eventBus.subscribe(
+      EventName.updated_rule_in_bot,
+      async ({ rule, botId }) => {
+        const jobId = this.getJobId(rule.id, botId);
+        this.cancelScheduledMessage(jobId);
 
-      if (bot) {
-        for (const rule of rules) {
+        const [bot] = await this.botsService.getBots({ ids: [botId] });
+        if (bot) {
           this.scheduleMessage(rule, bot);
         }
       }
-    });
-    this.eventBus.subscribe(EventName.removed_rules_from_bot, ({ ruleIds, botId }) => {
-      for (const ruleId of ruleIds) {
-        const jobId = this.getJobId(ruleId, botId);
-        this.cancelScheduledMessage(jobId);
-      } 
-    });
-    this.eventBus.subscribe(EventName.updated_rule_in_bot, async ({ rule, botId }) => {
-      const jobId = this.getJobId(rule.id, botId);
-      this.cancelScheduledMessage(jobId);
-
-      const [bot] = await this.botsService.getBots({ ids: [botId] });
-      if (bot) {
-        this.scheduleMessage(rule, bot);
-      }
-    });
+    );
     this.initScheduleMessages();
   }
 
@@ -54,7 +62,7 @@ export class ScheduleMessagesService {
     for (const bot of bots) {
       for (const ruleId of bot.ruleIds) {
         const [rule] = await this.rulesService.getRules({ ids: [ruleId] });
-        if (rule && rule.condition.type === 'schedule') {
+        if (rule && rule.condition.type === "schedule") {
           this.scheduleMessage(rule, bot);
         }
       }
@@ -62,40 +70,70 @@ export class ScheduleMessagesService {
   }
 
   private scheduleMessage(rule: Rule, bot: Bot): void {
-    if (rule.condition.type === 'schedule' && rule.condition.scheduleChatIds.length) {
+    const jobId = this.getJobId(rule.id, bot.id);
+    const isJobAlreadyExist = this.jobs.has(jobId);
+
+    if (isJobAlreadyExist) {
+      return;
+    }
+
+    if (
+      rule.condition.type === "schedule" &&
+      rule.condition.scheduleChatIds.length
+    ) {
       const scheduleRule = this.getScheduleRule(rule);
       const job = schedule.scheduleJob(scheduleRule, async () => {
-        if (rule.condition.type === 'schedule') {
-          const chats = await this.chatsService.getChats({ ids: rule.condition.scheduleChatIds });
+        if (rule.condition.type === "schedule") {
+          const chats = await this.chatsService.getChats({
+            ids: rule.condition.scheduleChatIds,
+          });
+
           for (const chat of chats) {
             const chatId = Number(chat?.chatId);
             try {
               await this.sendMessageResponse(rule.response, bot.token, chatId);
-            } catch(err) {
-              this.logger.errorLog(`Error then send schedule message: ${JSON.stringify(err)}`);
+            } catch (err) {
+              this.logger.errorLog(
+                `Error then send schedule message: ${JSON.stringify(err)}`
+              );
             }
           }
         } else {
-          this.logger.errorLog(`Rule: ${rule.name} with not schedule condition type: ${rule.condition.type}`);
+          this.logger.errorLog(
+            `Rule: ${rule.name} with not schedule condition type: ${rule.condition.type}`
+          );
         }
       });
 
-      const jobId = this.getJobId(rule.id, bot.id);
       this.jobs.set(jobId, job);
-      this.logger.infoLog(`Job with id: ${jobId} scheduled on time: ${job.nextInvocation()}`);
+      this.logger.infoLog(
+        `Job with id: ${jobId} scheduled on time: ${job.nextInvocation()}`
+      );
     }
   }
 
-  private async sendMessageResponse(response: RuleResponse, botToken: string, chatId: number): Promise<void> {
-    if (response.type === 'message') {
-      await this.messageResponseService.sendTextMessage(botToken, chatId, response.text);
+  private async sendMessageResponse(
+    response: RuleResponse,
+    botToken: string,
+    chatId: number
+  ): Promise<void> {
+    if (response.type === "message") {
+      await this.messageResponseService.sendTextMessage(
+        botToken,
+        chatId,
+        response.text
+      );
     }
 
-    if (response.type === 'sticker') {
-      await this.messageResponseService.sendStickerMessage(botToken, chatId, response.stickerId);
+    if (response.type === "sticker") {
+      await this.messageResponseService.sendStickerMessage(
+        botToken,
+        chatId,
+        response.stickerId
+      );
     }
 
-    if (response.type === 'random_joke') {
+    if (response.type === "random_joke") {
       await this.messageResponseService.sendRandomJoke(botToken, chatId);
     }
   }
@@ -103,13 +141,21 @@ export class ScheduleMessagesService {
   private getScheduleRule(rule: Rule): RecurrenceRule {
     const scheduleRule = new schedule.RecurrenceRule();
 
-    if (rule.condition.type === 'schedule' && rule.condition.schedule.type === 'weekly') {
-      scheduleRule.dayOfWeek = this.getDayOfWeekIndex(rule.condition.schedule.dayOfWeek);
+    if (
+      rule.condition.type === "schedule" &&
+      rule.condition.schedule.type === "weekly"
+    ) {
+      scheduleRule.dayOfWeek = this.getDayOfWeekIndex(
+        rule.condition.schedule.dayOfWeek
+      );
       scheduleRule.hour = rule.condition.schedule.hour;
       scheduleRule.minute = rule.condition.schedule.minute;
     }
 
-    if (rule.condition.type === 'schedule' && rule.condition.schedule.type === 'annually') {
+    if (
+      rule.condition.type === "schedule" &&
+      rule.condition.schedule.type === "annually"
+    ) {
       scheduleRule.month = rule.condition.schedule.month;
       scheduleRule.date = rule.condition.schedule.day;
       scheduleRule.hour = rule.condition.schedule.hour;
@@ -121,13 +167,20 @@ export class ScheduleMessagesService {
 
   private getDayOfWeekIndex(dayOfWeek: DayOfWeek): number {
     switch (dayOfWeek) {
-      case 'monday': return 1;
-      case 'tuesday': return 2;
-      case 'wednesday': return 3;
-      case 'thursday': return 4;
-      case 'friday': return 5;
-      case 'saturday': return 6;
-      case 'sunday': return 0;
+      case "monday":
+        return 1;
+      case "tuesday":
+        return 2;
+      case "wednesday":
+        return 3;
+      case "thursday":
+        return 4;
+      case "friday":
+        return 5;
+      case "saturday":
+        return 6;
+      case "sunday":
+        return 0;
     }
   }
 
